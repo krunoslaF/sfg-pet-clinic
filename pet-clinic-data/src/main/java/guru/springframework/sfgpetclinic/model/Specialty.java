3ad9d1125c8481a124fd9b4328d517e2c9269a75
kguldan <krunoslav.guldan@intellexi.hr>
2019-10-15 13:55:48 +0200

    adding Specialty entity

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author kguldan
+ */
+@Getter
+@Setter
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+}
